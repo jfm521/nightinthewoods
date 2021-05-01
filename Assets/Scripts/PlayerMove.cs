37bb23c93273b1be80966388ea8ba1f1e873a7c4
Jackie Murray <jfm521@nyu.edu>
2021-04-30 23:51:45 -0400

    stargazing works now

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -80,7 +80,7 @@ public class PlayerMove : MonoBehaviour
 
         HandleMovement();
         TimeTripleJump();
-        DetectCollisions();
+        //DetectCollisions();
     }
 
 
@@ -215,10 +215,10 @@ public class PlayerMove : MonoBehaviour
 
 
     // Detects for collision with the floor
-    void DetectCollisions()
+    /*void DetectCollisions()
     {
         RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector3(transform.position.x + myBody.velocity.x, transform.position.y + myBody.velocity.y), 1);
         Debug.DrawRay(transform.position, new Vector3(transform.position.x + myBody.velocity.x, transform.position.y + myBody.velocity.y), Color.black);
-    }
+    }*/
 
 }

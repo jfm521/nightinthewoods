9f44076fe2a896c5541f25f2a30ce098a189486e
DESKTOP-2I161N5\kathe <kl3542@nyu.edu>
2021-05-02 16:38:47 -0400

    why

---
+++ Assets/Scripts/sceneTransition.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class sceneTransition : MonoBehaviour
+{
+
+    public bool fading = false;
+    public SpriteRenderer blackScreen;
+    private Color fadeColor;
+    public float alpha = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (fading == true){
+            Debug.Log("fading"); //if its fading
+            
+            if (alpha < 1){ //if slowly adding color
+                alpha += .01f; //ad more
+            } else { //if not and completely solid
+                SceneManager.LoadScene("star gazing"); //change screen
+            }
+        } else {
+            if (alpha > 0){ //at start of scene
+                alpha -= .01f; //slowly fade dark screen away
+            }
+        }
+
+        fadeColor = new Color(0, 0, 0, alpha);
+        blackScreen.color = fadeColor;
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.gameObject.name == "transitionTriggerBox"){
+            //gameObject.GetComponent<SpriteRenderer>().enabled = false; makes player disappear
+            fading = true; //sets baclk screen to strat fading
+        }
+    }
+}
---
+++ Assets/Scripts/starConnect_BACKUP_6738.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class starConnect : MonoBehaviour
+{
+    //Stores how many stars each star is connected to
+    public int isConnected;
+
+    // Stores which stars can connect to the current star
+    public GameObject starFriend1;
+    public GameObject starFriend2;
+
+    // Stores which star is connected
+    public bool starFriend1Connected;
+    public bool starFriend2Connected;
+
+    // Stores the pointer
+    public GameObject pointer;
+
+<<<<<<< HEAD
+    // Line renderer
+    public LineRenderer myLine;
+    public LineRenderer connectedLine;
+
+    // Camera (for mouse pos)
+    public Camera cam;
+
+    // Debug
+    public bool debugOn;
+=======
+    // Stores the start and end points of the line
+    Vector3 startPt;
+    Vector3 endPt;
+>>>>>>> b5d3a358bb4d7c1a399287c179ad90cf1881520d
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isConnected = 0;
+
+        starFriend1Connected = false;
+        starFriend2Connected = false;
+<<<<<<< HEAD
+=======
+
+        startPt = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y);
+        endPt = startPt;
+>>>>>>> b5d3a358bb4d7c1a399287c179ad90cf1881520d
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+<<<<<<< HEAD
+        //Debug
+        if (debugOn)
+        {
+            Debug.Log(starFriend1Connected);
+            Debug.Log(starFriend2Connected);
+        }
+
+        //Detects if star friends are connected from a different star
+        if (starFriend1Connected == false && starFriend1.GetComponent<starConnect>().starFriend1Connected && starFriend1.GetComponent<starConnect>().starFriend1 == gameObject)
+        {
+            starFriend1Connected = true;
+        }
+        else if (starFriend1Connected == false && starFriend1.GetComponent<starConnect>().starFriend2Connected && starFriend1.GetComponent<starConnect>().starFriend2 == gameObject)
+        {
+            starFriend1Connected = true;
+        }
+        if (starFriend2Connected == false && starFriend2.GetComponent<starConnect>().starFriend1Connected && starFriend2.GetComponent<starConnect>().starFriend1 == gameObject)
+        {
+            starFriend2Connected = true;
+        }
+        else if (starFriend2Connected == false && starFriend2.GetComponent<starConnect>().starFriend2Connected && starFriend2.GetComponent<starConnect>().starFriend2 == gameObject)
+        {
+            starFriend2Connected = true;
+        }
+
+        //Draws the line if not yet connected
+        if (!starFriend1Connected || !starFriend2Connected)
+        {
+            if (pointer.GetComponent<pointerMove>().isConnecting && pointer.GetComponent<pointerMove>().starInHand == gameObject)
+            {
+                float mousex = cam.ScreenToWorldPoint(Input.mousePosition).x;
+                float mousey = cam.ScreenToWorldPoint(Input.mousePosition).y;
+                myLine.SetPosition(1, new Vector3(mousex - gameObject.transform.position.x, mousey - gameObject.transform.position.y));
+                if (debugOn)
+                {
+                    Debug.Log("Drawing line");
+                }
+            }
+            else
+            {
+                myLine.SetPosition(1, new Vector3(0, 0));
+            }
+        }
+        else
+        {
+            myLine.SetPosition(1, new Vector3(0, 0));
+        }
+
+        // Draws the line if connected
+        if (starFriend1Connected)
+        {
+            connectedLine.SetPosition(1, new Vector3(starFriend1.gameObject.transform.position.x - gameObject.transform.position.x, starFriend1.gameObject.transform.position.y - gameObject.transform.position.y));
+        }
+    }
+
+    // When mouse is clicked while touching star, connect or cancel
+=======
+        //Draws the line (HAVE GIZMOS ON)
+        if (pointer.GetComponent<pointerMove>().isConnecting && pointer.GetComponent<pointerMove>().starInHand == gameObject)
+        {
+
+        }
+        Debug.DrawLine(startPt, endPt);
+    }
+
+>>>>>>> b5d3a358bb4d7c1a399287c179ad90cf1881520d
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (collision.tag == "Pointer")
+            {
+                if (collision.GetComponent<pointerMove>().isConnecting)
+                {
+                    if (collision.GetComponent<pointerMove>().starInHand == starFriend1 && !starFriend1Connected)
+                    {
+                        isConnected += 1;
+                        starFriend1Connected = true;
+<<<<<<< HEAD
+
+=======
+>>>>>>> b5d3a358bb4d7c1a399287c179ad90cf1881520d
+                    }
+                    else if (collision.GetComponent<pointerMove>().starInHand == starFriend2 && !starFriend2Connected)
+                    {
+                        isConnected += 1;
+                        starFriend2Connected = true;
+                    }
+                }
+            }
+        }
+    }
+}
